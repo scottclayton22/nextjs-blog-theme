9f8ee29b05f3eb85e4f6cbfabc7fd869842e17c4
Scott Clayton <137470290+scottclayton22@users.noreply.github.com>
2023-11-11 06:29:25 -0800

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -7,6 +7,8 @@ class MyDocument extends Document {
       <Html lang="en" className="theme-compiled">
         <Head>
           <meta name="google-adsense-account" content="ca-pub-4129462129900088" />
+          <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4129462129900088"
+     crossorigin="anonymous"></script>
         </Head>
         <body
           className={`antialiased text-lg bg-white dark:bg-gray-900 dark:text-white leading-base`}
