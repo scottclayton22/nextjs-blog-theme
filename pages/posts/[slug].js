31020f9b8cef26f7a0fb731a46a35179e6b6f6c2
Scott Clayton <137470290+scottclayton22@users.noreply.github.com>
2023-11-11 06:38:25 -0800

    Update [slug].js

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -68,7 +68,6 @@ export default function PostPage({
           </article>
           <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4129462129900088"
      crossorigin="anonymous"></script>
-<!-- display_add1 -->
 <ins class="adsbygoogle"
      style="display:block"
      data-ad-client="ca-pub-4129462129900088"
